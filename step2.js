84c78750abc141eee2ca4b383a283d125e9b24e6
Peter Loft <peterloft3@gmail.com>
2022-08-16 17:51:18 -0500

    commiting packages, installed axios. step2, wrote logic to call either cat or webCat depending on if the argument starts with 'http' or not.

--- step2.js
+++ step2.js
@@ -5,34 +5,49 @@ const axios = require('axios')
 const argv = process.argv;
 
 /**
-* Function which accepts a path to a text file
-* and returns the contents of said text file.
+ * Function which accepts a path to a text file
+ * and returns the contents of said text file.
  * 
  * @param {string} path 
  */
 async function cat(path) {
+  let contents;
   try {
-    let contents = await fsP.readFile(path, "utf8");
-    console.log("Contents: ", contents)
+    contents = await fsP.readFile(path, "utf8");
   } catch (e) {
     console.log(e);
     process.exit(1);
   }
+  console.log("Contents: ", contents)
+}
+
+/**
+ * Function which accepts a URL to a webiste
+ * and returns the html contents of said website.
+ * @param {string} url 
+ */
+async function webCat(url) {
+  let contents;
+  try {
+    contents = await axios({url});
+  } catch (e) {
+    console.log(e);
+    process.exit(1);
+  }
+  console.log("Contents: ", contents.data);
 }
 
 /**
  * 
- * @param {string} url 
+ * @param {string} arg 
  */
-async function webCat(url) {
-  try {
-    let contents = await axios.get(url);
-    console.log("Contents: ", contents);
-  } catch (e) {
-    console.log(e);
-    process.exit(1);
+function callCats(arg) {
+  if (arg.startsWith('http')) {
+    webCat(arg)
+  } else {
+    cat(arg)
   }
 }
 
-cat(argv[2]);
+callCats(argv[2]);
 
