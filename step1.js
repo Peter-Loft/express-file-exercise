2feecc3a450d66d00c2348a00588d341d770f7f1
Peter Loft <peterloft3@gmail.com>
2022-08-16 17:16:00 -0500

    Completed the first request. Added a function call 'cat()' which accepts a string path to a text file. The file is read and logged to console.

---
+++ step1.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const fsP = require('fs/promises');
+const argv = process.argv;
+
+async function cat(path) {
+  try {
+    let contents = await fsP.readFile(path, "utf8");
+    console.log("Contents: ", contents)
+  } catch (e) {
+    process.exit(1);
+  }
+}
+
+cat(argv[2]);
+
